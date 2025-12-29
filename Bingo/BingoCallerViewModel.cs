5d4f76c406a3cc16b9a3200f7828777e9e968871
szigh <148691844+szigh@users.noreply.github.com>
2025-12-29 21:28:28 +0000

    Add a control to show called numbers

--- Bingo/BingoCallerViewModel.cs
+++ Bingo/BingoCallerViewModel.cs
@@ -1,48 +1,51 @@
-﻿using System.Windows.Input;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
-namespace Bingo
+namespace Bingo;
+
+public partial class BingoCallerViewModel : ObservableObject
 {
-    public class BingoCallerViewModel : ObservableObject
+    private readonly IBingoNumberGenerator _bingoNumberGenerator;
+
+    public BingoCallerViewModel(IBingoNumberGenerator bingoNumberGenerator)
     {
-        private readonly IBingoNumberGenerator _bingoNumberGenerator;
+        _bingoNumberGenerator = bingoNumberGenerator;
+        CallNextNumberCommand = new RelayCommand(CallNextNumber);
+        InitializeNumbersBoard();
+    }
 
-        public BingoCallerViewModel(IBingoNumberGenerator bingoNumberGenerator)
+    [ObservableProperty]
+    private string _currentCall = "";
+    
+    [ObservableProperty]
+    private string _info = "Ready to play";
+
+    public ICommand CallNextNumberCommand { get; }
+
+    public ObservableCollection<BingoNumberCell> NumbersBoard { get; } = [];
+
+    private void InitializeNumbersBoard()
+    {
+        for (int i = 1; i <= 90; i++)
+            NumbersBoard.Add(new BingoNumberCell(i));
+    }
+
+    private void CallNextNumber()
+    {
+        if (_bingoNumberGenerator.TryGetNextNumber(out var nextNumber))
         {
-            _bingoNumberGenerator = bingoNumberGenerator;
-            CallNextNumberCommand = new RelayCommand(CallNextNumber);
+            Info = "";
+            CurrentCall = $"{nextNumber}";
+            
+            var cell = NumbersBoard.FirstOrDefault(c => c.Number == nextNumber);
+            cell?.IsCalled = true;
         }
-
-        private string _currentCall = "";
-        private string _info = "Ready to play";
-
-        public ICommand CallNextNumberCommand { get; }
-
-        public string CurrentCall
+        else
         {
-            get => _currentCall;
-            set => SetProperty(ref _currentCall, value);
-        }
-
-        public string Info
-        {
-            get => _info;
-            set => SetProperty(ref _info, value);
-        }
-
-        private void CallNextNumber()
-        {
-            if (_bingoNumberGenerator.TryGetNextNumber(out var nextNumber))
-            {
-                Info = "";
-                CurrentCall = $"{nextNumber}";
-            }
-            else
-            {
-                Info = "All numbers have been called";
-                CurrentCall = "";
-            }
+            Info = "All numbers have been called";
+            CurrentCall = "";
         }
     }
 }
